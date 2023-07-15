728f48a27f6a00a174283ee6f96b49a88386b874
Alexcmad <mcintoshalexander80@gmail.com>
2023-07-14 21:07:15 -0500

    Make project accessible to doss an matt

---
+++ Learn the Basics/main.py
@@ -0,0 +1,43 @@
+"""Description: Create a student management system that allows users to perform basic operations such as adding,
+retrieving, updating, and deleting student records. The program should utilize various Python concepts,
+including variables, data types, lists, tuples, sets, dictionaries, conditionals, type casting, exceptions,
+functions, and built-in functions.
+
+Requirements:
+
+The program should provide a menu-driven interface with the following options:
+a. Add a new student record
+b. Retrieve a student record by ID
+c. Update a student record
+d. Delete a student record
+e. Display all student records
+f. Exit the program
+
+Each student record should contain the following information:
+
+- Student ID (integer)
+- Name (string)
+- Age (integer)
+- Grade (float)
+
+Use appropriate data structures to store the student records. For example, you can use a list of dictionaries,
+where each dictionary represents a student record.
+
+Implement error handling to handle potential exceptions such as invalid input or non-existent student IDs.
+
+Implement input validation to ensure that the user enters the correct data type for each field (e.g., integer for ID,
+string for name).
+
+Provide appropriate messages to inform the user of the outcome of each operation (e.g., "Student record added
+successfully," "Student record not found," etc.).
+
+Use built-in functions such as len(), input(), str(), int(), etc., as needed throughout the program.
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
