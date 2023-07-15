d1dab0bf7b3ad556320184bf3524cad333930ffa
Alexcmad <mcintoshalexander80@gmail.com>
2023-07-15 17:49:54 -0500

    Made student class

--- Learn the Basics/main.py
+++ Learn the Basics/main.py
@@ -34,12 +34,35 @@ successfully," "Student record not found," etc.).
 Use built-in functions such as len(), input(), str(), int(), etc., as needed throughout the program.
 """
 
+students_list = []
+
+
+class Student:
+    school = "Crawsis"
+
+    def __init__(self, name: str, age: int, grade: float, ID: int):
+        self.name = name
+        self.age = age
+        self.grade = grade
+        self.ID = ID
+
+
+    def __repr__(self):
+        return f"{self.name}:{self.ID}:{self.age}:{self.grade}"
+
+    def __str__(self):
+        return f"Name:{self.name}, ID:{self.ID}"
+
+    def __eq__(self, other):
+        return self.ID
+
 
 def main():
-    print ("Hello World")
-    print ("Doss loves you")
-    print ("Hitler was right")
-
+    """
+    alex = Student(name="alexander", age=20, grade=2, ID=69)
+    print(alex == 69)
+    doss = Student(name="Doss", age=18, grade=15, ID=20031)"""
+    # comment
 
 
 if __name__ == "__main__":
