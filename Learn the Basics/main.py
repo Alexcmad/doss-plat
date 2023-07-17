50c3b311ddcfb992f2d8a955fd671570526b6755
Alexcmad <mcintoshalexander80@gmail.com>
2023-07-17 01:11:18 -0500

    more cleanup and made the update and print fucntions Student class mehtods

--- Learn the Basics/main.py
+++ Learn the Basics/main.py
@@ -1,3 +1,5 @@
+import os
+
 from exceptions import InvalidOptionException
 
 students_list = []
@@ -12,6 +14,24 @@ class Student:
         self.grade = grade
         self.ID = ID
 
+    def print_info(self):
+        print(f"Name:{self.name} | Age:{self.age} | Grade:{self.grade} | ID:{self.ID}")
+
+    def update(self, name: str, age: str, grade: str, ID: str):
+        try:
+            if name:
+                self.name = name
+            if age:
+                self.age = int(age)
+            if grade:
+                self.grade = float(grade)
+            if ID:
+                self.ID = int(ID)
+
+            print("Student Record Updated Successfully")
+        except:
+            print("Error Updating Student Record")
+
     def __repr__(self):
         return f"{self.name}:{self.ID}:{self.age}:{self.grade}"
 
@@ -25,7 +45,8 @@ class Student:
 def main():
     function_dict = {
         1: add_student,
-        2: get_student,
+        2: find_student_by_id,
+        3: update_student_record,
         5: print_all_records
     }
 
@@ -55,6 +76,7 @@ def main():
 
     chosen_function = function_dict.get(choice)
     chosen_function()
+    os.system('cls' if os.name == 'nt' else 'clear')
     main()
 
 
@@ -68,30 +90,52 @@ def add_student():
     students_list.append(new_student)
 
 
-def get_student():
+def find_student_by_id():
     student_id = input("What is the id of the student you want to find?: ")
-    found_student = False
-    for student in students_list:
-        if int(student_id) == student.ID:
-            print(student.name, student.age, student.grade, student.ID)
-            found_student = True
-            break
-    if not found_student:
-        print(f"Student with ID of {student_id} not found")
+    student = get_student(student_id)
+
+    if student:
+        print(student)
 
     choice = input("Press enter to continue or s to search again: ")
     if choice.lower() == "s":
-        get_student()
+        find_student_by_id()
 
 
-def print_student_info(student):
-    print(student.name, student.age, student.grade, student.ID)
+def update_student_record():
+    student_id = input("Please enter the ID of the student whose record you wish to update: ")
+    student = get_student(student_id)
+
+    if student:
+        student.print_info()
+        print("For the following fields enter a new value or press enter to skip")
+        new_name = input("Name: ")
+        new_age = input("Age: ")
+        new_grade = input("Grade: ")
+        new_ID = input("ID: ")
+        student.update(name=new_name, age=new_age, ID=new_ID, grade=new_grade)
+        enter_continue()
+
+    choice = input("Press enter to continue or s to search again: ")
+    if choice.lower() == "s":
+        update_student_record()
+
+
+def enter_continue():
+    input("Press Enter to continue...")
+
+
+def get_student(student_id: str):
+    for student in students_list:
+        if int(student_id) == student.ID:
+            return student
+    print(f"Student with ID of {student_id} not found")
 
 
 def print_all_records():
     for student in students_list:
-        print_student_info(student)
-    input("Press enter to continue...")
+        student.print_info()
+    enter_continue()
 
 
 if __name__ == "__main__":
